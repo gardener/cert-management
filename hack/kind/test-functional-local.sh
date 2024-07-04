d2e3bbff00624adc4d1bf3d077ec7d6a6ac46b51
Martin Weindel <martin.weindel@sap.com>
2024-07-04 09:28:52 +0200

    adjusted ginkgo options

--- hack/kind/test-functional-local.sh
+++ hack/kind/test-functional-local.sh
@@ -7,4 +7,4 @@ source "$(dirname ${0})/common.sh" ''
 
 cd "$SOURCE_PATH/test/functional"
 
-FUNCTEST_CONFIG=functest-config-kind.yaml DNS_KUBECONFIG=$KUBECONFIG DNS_DOMAIN=functest.certman.kind USE_DNSRECORDS=$USE_DNSRECORDS ginkgo --output-interceptor-mode=none --succinct
+FUNCTEST_CONFIG=functest-config-kind.yaml DNS_KUBECONFIG=$KUBECONFIG DNS_DOMAIN=functest.certman.kind USE_DNSRECORDS=$USE_DNSRECORDS ginkgo --succinct
