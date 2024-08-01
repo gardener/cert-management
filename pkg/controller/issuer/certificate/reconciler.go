21f8def420a7099aef9c1b9e677ab63a01275fc6
RaphaelVogel <raphael.vogel@sap.com>
2024-08-01 13:54:09 +0200

    enable cert duration for acme issuer

--- pkg/controller/issuer/certificate/reconciler.go
+++ pkg/controller/issuer/certificate/reconciler.go
@@ -410,9 +410,7 @@ func (r *certReconciler) obtainCertificateAndPendingACME(logctx logger.LogContex
 	if err != nil {
 		return r.failed(logctx, obj, api.StateError, err)
 	}
-	if cert.Spec.Duration != nil {
-		return r.failedStop(logctx, obj, api.StateError, fmt.Errorf("duration cannot be set for ACME certificate"))
-	}
+
 	err = r.validateDomainsAndCsr(&cert.Spec, issuer.Spec.ACME.Domains, issuerKey)
 	if err != nil {
 		return r.failedStop(logctx, obj, api.StateError, err)
@@ -511,6 +509,7 @@ func (r *certReconciler) obtainCertificateAndPendingACME(logctx logger.LogContex
 		AlwaysDeactivateAuthorizations: r.alwaysDeactivateAuthorizations,
 		PreferredChain:                 preferredChain,
 		KeyType:                        keyType,
+		Duration:                       duration,
 	}
 
 	err = r.obtainer.Obtain(input)
