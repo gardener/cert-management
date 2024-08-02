6ce80fd6ca458f3e890af588741556fc90887383
Martin Weindel <martin.weindel@sap.com>
2024-08-02 08:44:41 +0200

    draft caHandler

---
+++ pkg/certman2/core/const.go
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company and Gardener contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package core
+
+const (
+	// ACMEType is the type name for ACME.
+	ACMEType = "acme"
+	// CAType is the type name for CA.
+	CAType = "ca"
+)
