1f4034b204a208db1b5dba6bdf02a27483dcc46b
Thomas Wall <thomaswall@gmx.net>
2025-01-21 16:54:57 +0100

    test: enhance domain range tests with additional cases and refactor descriptions

--- pkg/cert/utils/domainrange_test.go
+++ pkg/cert/utils/domainrange_test.go
@@ -12,8 +12,8 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("IsInDomainRange", func() {
-	DescribeTable("domain range tests",
+var _ = Describe("DomainRange", func() {
+	DescribeTable("IsInDomainRange",
 		func(domain, domainRange string, wanted bool) {
 			domainRange = utils.NormalizeDomainRange(domainRange)
 			result := utils.IsInDomainRange(domain, domainRange)
@@ -30,5 +30,36 @@ var _ = Describe("IsInDomainRange", func() {
 		Entry("a.xb.c not in b.c", "a.xb.c", "b.c", false),
 		Entry("a.b in b.", "a.b", "b.", true),
 		Entry("a.b. in b", "a.b.", "b", true),
+		Entry("Empty domain range acts as wildcard", "a.b.c", "", true),
 	)
+
+	DescribeTable("IsInDomainRanges",
+		func(domain string, domainRanges []string, wanted bool) {
+			for i, domainRange := range domainRanges {
+				domainRanges[i] = utils.NormalizeDomainRange(domainRange)
+			}
+			result := utils.IsInDomainRanges(domain, domainRanges)
+			Expect(result).To(Equal(wanted))
+		},
+		Entry("a.b in {b}", "a.b", []string{"b"}, true),
+		Entry("a.b in {b, c}", "a.b", []string{"b", "c"}, true),
+		Entry("a.b in {c, b}", "a.b", []string{"c", "b"}, true),
+		Entry("a.b not in {c, d}", "a.b", []string{"c", "d"}, false),
+		Entry("Nil acts as wildcard", "a.b", nil, true),
+	)
+
+	DescribeTable("BestDomainRange",
+		func(domain string, domainRanges []string, wanted string) {
+			for i, domainRange := range domainRanges {
+				domainRanges[i] = utils.NormalizeDomainRange(domainRange)
+			}
+			result := utils.BestDomainRange(domain, domainRanges)
+			Expect(result).To(Equal(wanted))
+		},
+		Entry("a.b in {b} returns b", "a.b", []string{"b"}, "b"),
+		Entry("a.b in {b, c} returns b", "a.b", []string{"b", "c"}, "b"),
+		Entry("a.b.c in {b.c., c} returns b.c", "a.b.c", []string{"b.c", "c"}, "b.c"),
+		Entry("Nil acts as wildcard", "a.b", nil, "*"),
+	)
+
 })
