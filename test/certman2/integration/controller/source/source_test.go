25d3860995bf8a1cba7fd965a5fa8c7c633ae452
Martin Weindel <martin.weindel@sap.com>
2025-02-25 12:14:57 +0100

    SkipNameValidation

--- test/certman2/integration/controller/source/source_test.go
+++ test/certman2/integration/controller/source/source_test.go
@@ -24,6 +24,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+	controllerconfig "sigs.k8s.io/controller-runtime/pkg/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	gatewayapisv1 "sigs.k8s.io/gateway-api/apis/v1"
@@ -100,6 +101,9 @@ var _ = Describe("Source controller tests", func() {
 					},
 					&apiextensionsv1.CustomResourceDefinition{}: {},
 				},
+			},
+			Controller: controllerconfig.Controller{
+				SkipNameValidation: ptr.To(true),
 			},
 		})
 		Expect(err).NotTo(HaveOccurred())
