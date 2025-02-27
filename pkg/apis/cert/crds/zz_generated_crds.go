573b4957899494890d5000a8c0d3ef68026909b0
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-02-27 08:48:06 +0100

    fix(deps): update module github.com/gardener/gardener to v1.113.0 (#424)
    * fix(deps): update module github.com/gardener/gardener to v1.113.0
    
    * chore: Run and apply `make generate-renovate-ignore-deps`
    
    * chore: Update Go to 1.24
    
    * make generate
    
    ---------
    
    Co-authored-by: Marc Vornetran <marc.vornetran@sap.com>
    Co-authored-by: Martin Weindel <martin.weindel@sap.com>

--- pkg/apis/cert/crds/zz_generated_crds.go
+++ pkg/apis/cert/crds/zz_generated_crds.go
@@ -19,7 +19,7 @@ apiVersion: apiextensions.k8s.io/v1
 kind: CustomResourceDefinition
 metadata:
   annotations:
-    controller-gen.kubebuilder.io/version: v0.16.5
+    controller-gen.kubebuilder.io/version: v0.17.2
   name: certificaterevocations.cert.gardener.cloud
 spec:
   group: cert.gardener.cloud
@@ -307,7 +307,7 @@ apiVersion: apiextensions.k8s.io/v1
 kind: CustomResourceDefinition
 metadata:
   annotations:
-    controller-gen.kubebuilder.io/version: v0.16.5
+    controller-gen.kubebuilder.io/version: v0.17.2
   name: certificates.cert.gardener.cloud
 spec:
   group: cert.gardener.cloud
@@ -708,7 +708,7 @@ apiVersion: apiextensions.k8s.io/v1
 kind: CustomResourceDefinition
 metadata:
   annotations:
-    controller-gen.kubebuilder.io/version: v0.16.5
+    controller-gen.kubebuilder.io/version: v0.17.2
   name: issuers.cert.gardener.cloud
 spec:
   group: cert.gardener.cloud
