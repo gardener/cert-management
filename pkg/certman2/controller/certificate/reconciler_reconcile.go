f4f21d1fa8736343be7f8ad1665008ea278c9dbf
Marc Vornetran <marc.vornetran@sap.com>
2025-04-23 15:44:04 +0200

    refactor: Handle orphaned pending certificate

--- pkg/certman2/controller/certificate/reconciler_reconcile.go
+++ pkg/certman2/controller/certificate/reconciler_reconcile.go
@@ -17,13 +17,40 @@ import (
 func (r *Reconciler) reconcile(
 	ctx context.Context,
 	log logr.Logger,
-	landscape *v1alpha1.Certificate,
+	cert *v1alpha1.Certificate,
 ) (
 	reconcile.Result,
 	error,
 ) {
-	log.Info("reconcile certificate")
-	_ = ctx
-	_ = landscape
+	log.Info("reconcile cert")
+
+	if r.isOrphanedPendingCertificate(cert) {
+		return r.handleOrphanedPendingCertificate(ctx, cert)
+	}
+
 	return reconcile.Result{}, fmt.Errorf("not yet supported")
+}
+
+func (r *Reconciler) isOrphanedPendingCertificate(cert *v1alpha1.Certificate) bool {
+	return cert.Status.State == v1alpha1.StatePending && !r.hasPendingChallenge(cert) && !r.hasResultPending(cert)
+}
+
+// handleOrphanedPendingCertificate cleans up invalid orphaned pending state unfinished from former controller instance, resets status to trigger a retry.
+func (r *Reconciler) handleOrphanedPendingCertificate(ctx context.Context, cert *v1alpha1.Certificate) (reconcile.Result, error) {
+	cert.Status.LastPendingTimestamp = nil
+	err := r.Client.Update(ctx, cert)
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("failed to update certificate status: %w", err)
+	}
+	return reconcile.Result{}, nil
+}
+
+func (r *Reconciler) hasPendingChallenge(_ *v1alpha1.Certificate) bool {
+	// TODO
+	return false
+}
+
+func (r *Reconciler) hasResultPending(_ *v1alpha1.Certificate) bool {
+	// TODO
+	return false
 }
