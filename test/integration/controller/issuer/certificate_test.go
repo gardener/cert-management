6f944e349271013c58ad58f37d1b9d36b360bfed
Marc Vornetran <marc.vornetran@sap.com>
2025-07-07 18:01:39 +0200

    test: Extend integration tests

--- test/integration/controller/issuer/certificate_test.go
+++ test/integration/controller/issuer/certificate_test.go
@@ -6,8 +6,13 @@ package issuer_test
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/pem"
+	"net"
+	"net/url"
 	"time"
 
 	. "github.com/gardener/gardener/pkg/utils/test/matchers"
@@ -123,6 +128,133 @@ var _ = Describe("Certificate controller tests", func() {
 			expired, _ := time.Parse(time.RFC3339, *expirationDate)
 			Expect(issued).To(BeTemporally("<", expired))
 		})
+
+		It("should be properly created with subject alternative names (SANs)", func() {
+			By("Create self-signed certificate with SANs")
+			certificate := &certv1alpha1.Certificate{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace:    testNamespace.Name,
+					GenerateName: "self-signed-certificate-with-sans-",
+				},
+				Spec: certv1alpha1.CertificateSpec{
+					IssuerRef: &certv1alpha1.IssuerRef{
+						Namespace: selfSignedIssuer.Namespace,
+						Name:      selfSignedIssuer.Name,
+					},
+					IsCA:           ptr.To(true),
+					CommonName:     ptr.To("example.com"),
+					EmailAddresses: []string{"foo@example.com", "bar@example.com"},
+					IPAddresses:    []string{"1.1.1.1", "1.0.0.1"},
+					URIs:           []string{"ftp://ftp.example.com", "urn:isbn:9780718097914"},
+				},
+			}
+			Expect(testClient.Create(ctx, certificate)).To(Succeed())
+			DeferCleanup(func() {
+				Expect(testClient.Delete(ctx, certificate)).To(Succeed())
+			})
+			Eventually(func(g Gomega) {
+				g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(certificate), certificate)).To(Succeed())
+				g.Expect(certificate.Status.State).To(Equal("Ready"))
+			}).WithTimeout(10 * time.Second).Should(Succeed())
+
+			By("Read certificate secret")
+			secret := &corev1.Secret{}
+			err := testClient.Get(ctx, client.ObjectKey{
+				Namespace: certificate.Spec.SecretRef.Namespace,
+				Name:      certificate.Spec.SecretRef.Name,
+			}, secret)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Decode certificate from secret")
+			certBlock, _ := pem.Decode(secret.Data[corev1.TLSCertKey])
+			Expect(certBlock).NotTo(BeNil())
+			parsedCert, err := x509.ParseCertificate(certBlock.Bytes)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Check SANs in the certificate")
+			Expect(parsedCert.EmailAddresses).To(ConsistOf("foo@example.com", "bar@example.com"))
+			Expect(parsedCert.IPAddresses).To(ConsistOf(net.ParseIP("1.1.1.1").To4(), net.ParseIP("1.0.0.1").To4()))
+			Expect(parsedCert.URIs).To(ConsistOf(&url.URL{Scheme: "ftp", Host: "ftp.example.com"}, &url.URL{Scheme: "urn", Opaque: "isbn:9780718097914"}))
+		})
+
+		It("should be properly created with SANs through a certificate signing request (CSR)", func() {
+			By("Create a CSR")
+			privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+			Expect(err).NotTo(HaveOccurred())
+			certificateRequest := x509.CertificateRequest{
+				Subject: pkix.Name{
+					CommonName: "example.com",
+				},
+				EmailAddresses: []string{"foo@example.com", "bar@example.com"},
+				IPAddresses:    []net.IP{net.ParseIP("1.1.1.1").To4(), net.ParseIP("1.0.0.1").To4()},
+				URIs:           []*url.URL{{Scheme: "ftp", Host: "ftp.example.com"}, {Scheme: "urn", Opaque: "isbn:9780718097914"}},
+			}
+			csrDER, err := x509.CreateCertificateRequest(rand.Reader, &certificateRequest, privateKey)
+			Expect(err).NotTo(HaveOccurred())
+			csrPEM := pem.EncodeToMemory(&pem.Block{
+				Type:  "CERTIFICATE REQUEST",
+				Bytes: csrDER,
+			})
+
+			By("Create self-signed certificate with SANs through CSR")
+			certificate := &certv1alpha1.Certificate{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace:    testNamespace.Name,
+					GenerateName: "self-signed-certificate-with-sans-csr-",
+				},
+				Spec: certv1alpha1.CertificateSpec{
+					IssuerRef: &certv1alpha1.IssuerRef{
+						Namespace: selfSignedIssuer.Namespace,
+						Name:      selfSignedIssuer.Name,
+					},
+					IsCA: ptr.To(true),
+					CSR:  csrPEM,
+				},
+			}
+			Expect(testClient.Create(ctx, certificate)).To(Succeed())
+			DeferCleanup(func() {
+				Expect(testClient.Delete(ctx, certificate)).To(Succeed())
+			})
+			Eventually(func(g Gomega) {
+				g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(certificate), certificate)).To(Succeed())
+				g.Expect(certificate.Status.State).To(Equal("Ready"))
+			}).WithTimeout(10 * time.Second).Should(Succeed())
+		})
+
+		DescribeTable("should reject invalid SAN values",
+			func(emailAddresses []string, ipAddresses []string, uris []string, expectedErrorMessage string) {
+				By("Create self-signed certificate with SANs")
+				certificate := &certv1alpha1.Certificate{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace:    testNamespace.Name,
+						GenerateName: "self-signed-certificate-with-sans-",
+					},
+					Spec: certv1alpha1.CertificateSpec{
+						IssuerRef: &certv1alpha1.IssuerRef{
+							Namespace: selfSignedIssuer.Namespace,
+							Name:      selfSignedIssuer.Name,
+						},
+						IsCA:           ptr.To(true),
+						CommonName:     ptr.To("example.com"),
+						EmailAddresses: emailAddresses,
+						IPAddresses:    ipAddresses,
+						URIs:           uris,
+					},
+				}
+				Expect(testClient.Create(ctx, certificate)).To(Succeed())
+				DeferCleanup(func() {
+					Expect(testClient.Delete(ctx, certificate)).To(Succeed())
+				})
+				Eventually(func(g Gomega) {
+					g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(certificate), certificate)).To(Succeed())
+					g.Expect(certificate.Status.State).To(Equal("Error"))
+					g.Expect(*certificate.Status.Message).To(ContainSubstring(expectedErrorMessage))
+				}).WithTimeout(5 * time.Second).Should(Succeed())
+			},
+			Entry("invalid email", []string{"invalid-email"}, nil, nil, "invalid email address"),
+			Entry("invalid ip", nil, []string{"invalid-ip"}, nil, "invalid IP address"),
+			Entry("invalid uri", nil, nil, []string{":foo/bar"}, "invalid URI"),
+		)
 	})
 
 	Context("Self-signed certificates from CA issuer", func() {
@@ -256,5 +388,101 @@ var _ = Describe("Certificate controller tests", func() {
 			Entry("RSA 3072", certv1alpha1.RSAKeyAlgorithm, 3072),
 			Entry("RSA 4096", certv1alpha1.RSAKeyAlgorithm, 4096),
 		)
+
+		It("should be properly created with subject alternative names (SANs)", func() {
+			By("Create certificate from CA with SANs")
+			certificate := &certv1alpha1.Certificate{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace:    testNamespace.Name,
+					GenerateName: "ca-certificate-with-sans-",
+				},
+				Spec: certv1alpha1.CertificateSpec{
+					IssuerRef: &certv1alpha1.IssuerRef{
+						Namespace: caIssuer.Namespace,
+						Name:      caIssuer.Name,
+					},
+					CommonName: ptr.To("example.com"),
+					Duration: &metav1.Duration{
+						Duration: 48 * time.Hour,
+					},
+					EmailAddresses: []string{"foo@example.com", "bar@example.com"},
+					IPAddresses:    []string{"1.1.1.1", "1.0.0.1"},
+					URIs:           []string{"ftp://ftp.example.com", "urn:isbn:9780718097914"},
+				},
+			}
+			Expect(testClient.Create(ctx, certificate)).To(Succeed())
+			DeferCleanup(func() {
+				Expect(testClient.Delete(ctx, certificate)).To(Succeed())
+			})
+			Eventually(func(g Gomega) {
+				g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(certificate), certificate)).To(Succeed())
+				g.Expect(certificate.Status.State).To(Equal("Ready"))
+			}).WithTimeout(10 * time.Second).Should(Succeed())
+
+			By("Read certificate secret")
+			secret := &corev1.Secret{}
+			err := testClient.Get(ctx, client.ObjectKey{
+				Namespace: certificate.Spec.SecretRef.Namespace,
+				Name:      certificate.Spec.SecretRef.Name,
+			}, secret)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Decode certificate from secret")
+			certBlock, _ := pem.Decode(secret.Data[corev1.TLSCertKey])
+			Expect(certBlock).NotTo(BeNil())
+			parsedCert, err := x509.ParseCertificate(certBlock.Bytes)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Check SANs in the certificate")
+			Expect(parsedCert.EmailAddresses).To(ConsistOf("foo@example.com", "bar@example.com"))
+			Expect(parsedCert.IPAddresses).To(ConsistOf(net.ParseIP("1.1.1.1").To4(), net.ParseIP("1.0.0.1").To4()))
+			Expect(parsedCert.URIs).To(ConsistOf(&url.URL{Scheme: "ftp", Host: "ftp.example.com"}, &url.URL{Scheme: "urn", Opaque: "isbn:9780718097914"}))
+		})
+
+		It("should be properly created with SANs through a certificate signing request (CSR)", func() {
+			By("Create a CSR")
+			privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+			Expect(err).NotTo(HaveOccurred())
+			certificateRequest := x509.CertificateRequest{
+				Subject: pkix.Name{
+					CommonName: "example.com",
+				},
+				EmailAddresses: []string{"foo@example.com", "bar@example.com"},
+				IPAddresses:    []net.IP{net.ParseIP("1.1.1.1").To4(), net.ParseIP("1.0.0.1").To4()},
+				URIs:           []*url.URL{{Scheme: "ftp", Host: "ftp.example.com"}, {Scheme: "urn", Opaque: "isbn:9780718097914"}},
+			}
+			csrDER, err := x509.CreateCertificateRequest(rand.Reader, &certificateRequest, privateKey)
+			Expect(err).NotTo(HaveOccurred())
+			csrPEM := pem.EncodeToMemory(&pem.Block{
+				Type:  "CERTIFICATE REQUEST",
+				Bytes: csrDER,
+			})
+
+			By("Create certificate from CA with SANs through CSR")
+			certificate := &certv1alpha1.Certificate{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace:    testNamespace.Name,
+					GenerateName: "ca-certificate-with-sans-csr-",
+				},
+				Spec: certv1alpha1.CertificateSpec{
+					IssuerRef: &certv1alpha1.IssuerRef{
+						Namespace: caIssuer.Namespace,
+						Name:      caIssuer.Name,
+					},
+					Duration: &metav1.Duration{
+						Duration: 48 * time.Hour,
+					},
+					CSR: csrPEM,
+				},
+			}
+			Expect(testClient.Create(ctx, certificate)).To(Succeed())
+			DeferCleanup(func() {
+				Expect(testClient.Delete(ctx, certificate)).To(Succeed())
+			})
+			Eventually(func(g Gomega) {
+				g.Expect(testClient.Get(ctx, client.ObjectKeyFromObject(certificate), certificate)).To(Succeed())
+				g.Expect(certificate.Status.State).To(Equal("Ready"))
+			}).WithTimeout(10 * time.Second).Should(Succeed())
+		})
 	})
 })
