743e4896fb9cc334f45fba57bf8cf9bc9cd98563
Marc Vornetran <marc.vornetran@sap.com>
2025-08-20 15:29:02 +0200

    docs: Fix typo in `Certificate.spec.FollowCNAME` description

--- pkg/apis/cert/v1alpha1/types.go
+++ pkg/apis/cert/v1alpha1/types.go
@@ -83,7 +83,7 @@ type CertificateSpec struct {
 	// EnsureRenewedAfter specifies a time stamp in the past. Renewing is only triggered if certificate notBefore date is before this date.
 	// +optional
 	EnsureRenewedAfter *metav1.Time `json:"ensureRenewedAfter,omitempty"`
-	// FollowCNAME if true delegated domain for DNS01 challenge is used if CNAME record for DNS01 challange domain `_acme-challenge.<domain>` is set.
+	// FollowCNAME if true delegated domain for DNS01 challenge is used if CNAME record for DNS01 challenge domain `_acme-challenge.<domain>` is set.
 	// +optional
 	FollowCNAME *bool `json:"followCNAME,omitempty"`
 	// Keystores configures additional keystore output formats stored in the `secretName`/`secretRef` Secret resource.
